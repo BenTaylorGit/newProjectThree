7a65434e926f813fa59cd47d853dbd0372c0daf4
Ben Taylor <bentaylor@knights.ucf.edu>
2020-03-27 16:26:56 -0400

    Added ability to update user information, bio, website, location, added authebtication to update

--- projectThree-functions/functions/index.js
+++ projectThree-functions/functions/index.js
@@ -2,18 +2,21 @@ const functions = require("firebase-functions");
 
 const app = require("express")();
 
-const FBAuth = require('./util/fbAuth')
+const FBAuth = require('./util/fbAuth');
 
 const {getAllToots, postOneToot} = require('./handlers/toots');
-const {signUp, login, uploadImage} = require('./handlers/users');
+const {signUp, login, uploadImage, addUserDetails, getAuthenticatedUser} = require('./handlers/users');
 
 //toot routes
 app.get("/toots", getAllToots);
 app.post("/toot", FBAuth, postOneToot);
 
+
 //Users Route
 app.post("/signup", signUp);
 app.post('/login', login);
 app.post('/user/image', FBAuth, uploadImage);
+app.post('/user', FBAuth, addUserDetails);
+app.get('/user', FBAuth, getAuthenticatedUser);
 
 exports.api = functions.https.onRequest(app);
