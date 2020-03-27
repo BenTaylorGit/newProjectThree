f1872546c94cb2748e13f9cd7dcc5c8832c65957
Ben Taylor <bentaylor@knights.ucf.edu>
2020-03-27 12:28:48 -0400

    installed expressed set up new routes, test to firebase

--- projectThree-functions/functions/index.js
+++ projectThree-functions/functions/index.js
@@ -1,43 +1,55 @@
-const functions = require('firebase-functions');
-const admin = require('firebase-admin');
+const functions = require("firebase-functions");
+const admin = require("firebase-admin");
 
 admin.initializeApp();
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-exports.helloWorld = functions.https.onRequest((request, response) => {
- response.send("Hello World!");
-});
 
-exports.getToots = functions.https.onRequest((req, res) => {
-    admin.firestore().collection('toots').get()
+const express = require('express');
+const app = express();
+
+
+app.get('/toots', (req, res)=> {
+    admin
+    .firestore()
+    .collection("toots")
+    .orderBy('createdAt', 'desc')
+    .get()
     .then(data => {
-        data.forEach(doc => {
-            let toots = [];
-            data.forEach(doc => {
-                toots.push(doc.data());
+      data
+        .forEach(doc => {
+          let toots = [];
+          data.forEach(doc => {
+            toots.push({
+                tootId: doc.id,
+                body: doc.data().body,
+                userHandle: doc.data().userHandle,
+                createdAt: doc.data().createdAt
             });
-            return res.json(toots);
+          });
+          return res.json(toots);
         })
         .catch(err => console.error(err));
-    })
+    });
 });
 
-exports.createToot = functions.https.onRequest((req, res) => {
-    const newToot = {
-        body: req.body.body,
-        userHandle: req.body.userHandle,
-        createdAt: admin.firestore.Timestamp.fromDate(new Date())
-
-    };
-    admin.firestore()
-    .collection('toots')
+app.post('/toot', (req, res) => {
+    
+  const newToot = {
+    body: req.body.body,
+    userHandle: req.body.userHandle,
+    createdAt: new Date().toISOString()
+  };
+  admin
+    .firestore()
+    .collection("toots")
     .add(newToot)
-    .then((doc) => {
-        res.json({message: `document ${doc.id} created successfully`});
+    .then(doc => {
+      res.json({ message: `document ${doc.id} created successfully` });
     })
-    .catch((err) => {
-        res.status(500).json({error: `something went wrong`});
-        console.error(err);
+    .catch(err => {
+      res.status(500).json({ error: `something went wrong` });
+      console.error(err);
     });
-    });
+});
+
+
+exports.api = functions.https.onRequest(app);
