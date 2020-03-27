68b4742838dcadc5bf37d2e21e3a3fe080156b08
Ben Taylor <bentaylor@knights.ucf.edu>
2020-03-27 19:27:45 -0400

    added notification system for users

--- projectThree-functions/functions/index.js
+++ projectThree-functions/functions/index.js
@@ -4,8 +4,10 @@ const app = require("express")();
 
 const FBAuth = require('./util/fbAuth');
 
+const { db } = require('./util/admin');
+
 const {getAllToots, postOneToot, getToot, commentOnToot, likeToot, unlikeToot, deleteToot} = require('./handlers/toots');
-const {signUp, login, uploadImage, addUserDetails, getAuthenticatedUser} = require('./handlers/users');
+const {signUp, login, uploadImage, addUserDetails, getAuthenticatedUser, getUserDetails, markNotificationsRead} = require('./handlers/users');
 
 //toot routes
 app.get('/toots', getAllToots);
@@ -23,5 +25,64 @@ app.post('/login', login);
 app.post('/user/image', FBAuth, uploadImage);
 app.post('/user', FBAuth, addUserDetails);
 app.get('/user', FBAuth, getAuthenticatedUser);
+app.get('/user/:handle', getUserDetails);
+app.post('/notifications', FBAuth, markNotificationsRead);
 
 exports.api = functions.https.onRequest(app);
+
+exports.createNotificationOnLike = functions
+  
+  .firestore.document('likes/{id}')
+  .onCreate((snapshot) => {
+    return db
+      .doc(`/toots/${snapshot.data().tootId}`)
+      .get()
+      .then((doc) => {
+        if (doc.exists && doc.data().userHandle !== snapshot.data().userHandle) {
+          return db.doc(`/notifications/${snapshot.id}`).set({
+            createdAt: new Date().toISOString(),
+            recipient: doc.data().userHandle,
+            sender: snapshot.data().userHandle,
+            type: 'like',
+            read: false,
+            tootId: doc.id
+          });
+        }
+      })
+      .catch((err) => console.error(err));
+  });
+
+
+exports.deleteNotificationOnUnLike = functions
+  
+  .firestore.document('likes/{id}').onDelete((snapshot) => {
+    return db.doc(`/notifications/${snapshot.id}`).delete()
+      .catch((err) => {
+        console.error(err);
+        return;
+      });
+  });
+
+
+exports.createNotificationOnComment = functions
+  
+  .firestore.document('comments/{id}').onCreate((snapshot) => {
+    return db.doc(`/toots/${snapshot.data().tootId}`).get()
+      .then((doc) => {
+        if (doc.exists && doc.data().userHandle !== snapshot.data().userHandle
+        ) {
+          return db.doc(`/notifications/${snapshot.id}`).set({
+            createdAt: new Date().toISOString(),
+            recipient: doc.data().userHandle,
+            sender: snapshot.data().userHandle,
+            type: 'comment',
+            read: false,
+            tootId: doc.id
+          });
+        }
+      })
+      .catch((err) => {
+        console.error(err);
+        return;
+      });
+  });
